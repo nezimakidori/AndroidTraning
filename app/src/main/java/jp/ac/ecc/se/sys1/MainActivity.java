e4622966d5d8a329ce883ac8cc39288d461a7677
nezimakidori <karasawa2220256@gmail.com>
2022-10-28 14:39:36 +0900

    さいしょのこみっと

---
+++ app/src/main/java/jp/ac/ecc/se/sys1/MainActivity.java
@@ -0,0 +1,16 @@
+package jp.ac.ecc.se.sys1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("はじめての変更");
+    }
+}
